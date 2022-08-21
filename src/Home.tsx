3d879e0eeb963a9071b6c4bbdcab5f1dc9558a6a
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-22 01:01:58 +0900

    [update] アイコン画像の追加 タグを選択するメニューにアイコンを追加

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,11 @@ import * as React from "react";
 import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
 import Page from "./components/Page";
 
+import Sightseeing from "./../static/images/tag_menu/sightseeing.svg";
+import Gourmet from "./../static/images/tag_menu/gourmet.svg";
+import Walking from "./../static/images/tag_menu/walking.svg";
+import Souvenir from "./../static/images/tag_menu/souvenir.svg";
+
 import "./../static/css/home.scss";
 
 const ROOT_URL = "https://";
@@ -13,6 +18,7 @@ const options: AxiosRequestConfig = {
     method: "GET" 
 }
 
+// ページのデータ型を定義
 interface Page{
     image: string
     title: string
@@ -28,6 +34,7 @@ const page: Page = {
 const Home: React.FC = ()=>{
     const [page_names, setPageNames] = React.useState<Page[]>([]);
 
+    // viewをエンコードしたあとにページの情報を取得
     React.useEffect(() => {
         axios(options)
         .then((respons: AxiosResponse<Page[]>) => {
@@ -46,10 +53,10 @@ const Home: React.FC = ()=>{
         <div className="home">
             <div className="tag-menu-block">
                 <ul>
-                    <li><div></div><p>観光地</p></li>
-                    <li><div></div><p>グルメ</p></li>
-                    <li><div></div><p>たまさんぽ</p></li>
-                    <li><div></div><p>お土産</p></li>
+                    <li><div id="sightseeing"><Sightseeing /></div><p>観光地</p></li>
+                    <li><div id="gourumet"><Gourmet /></div><p>グルメ</p></li>
+                    <li><div id="walking"><Walking /></div><p>たまさんぽ</p></li>
+                    <li><div id="omiyage"><Souvenir /></div><p>お土産</p></li>
                 </ul>
             </div>
             <div className="pictures-blck">
