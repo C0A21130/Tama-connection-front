473225469aacacb4d43853ea32a275e9894e2cfe
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-22 17:00:32 +0900

    [update] ボタンを押した際にタグのstateを変更するように変更

--- src/Home.tsx
+++ src/Home.tsx
@@ -12,11 +12,6 @@ import "./../static/css/home.scss";
 const ROOT_URL = "https://";
 const PICTTUR_NUM = 2;
 const PAGE_NUM = 4;
- 
-const options: AxiosRequestConfig = {
-    url: `${ROOT_URL}/page`,
-    method: "GET" 
-}
 
 // ページのデータ型を定義
 interface Page{
@@ -25,6 +20,9 @@ interface Page{
     text: string
 }
 
+type Tag = "kankou" | "gurume" | "tamasanpo" | "omiyage";
+ 
+// 仮データ
 const page: Page = {
     "image" : "",
     "title" : "title",
@@ -32,7 +30,14 @@ const page: Page = {
 }
 
 const Home: React.FC = ()=>{
+
+    const [tag, setTag] = React.useState<Tag>("kankou")
     const [page_names, setPageNames] = React.useState<Page[]>([]);
+
+    const options: AxiosRequestConfig<Tag> = {
+        url: `${ROOT_URL}/page?tag=${tag}`,
+        method: "GET"
+    }
 
     // viewをエンコードしたあとにページの情報を取得
     React.useEffect(() => {
@@ -48,15 +53,20 @@ const Home: React.FC = ()=>{
 
     }, [])
 
+    // タグメニューのボタンを押したときにタグを切り替える
+    const change_tag = (t: Tag) => {
+        setTag(t)
+        console.log(tag)
+    }
 
     return(
         <div className="home">
             <div className="tag-menu-block">
                 <ul>
-                    <li><div id="sightseeing"><Sightseeing /></div><p>観光地</p></li>
-                    <li><div id="gourumet"><Gourmet /></div><p>グルメ</p></li>
-                    <li><div id="walking"><Walking /></div><p>たまさんぽ</p></li>
-                    <li><div id="omiyage"><Souvenir /></div><p>お土産</p></li>
+                    <li onClick={() => change_tag("kankou")}><div id="sightseeing"><Sightseeing /></div><p>観光地</p></li>
+                    <li onClick={() => change_tag("gurume")}><div id="gourumet"><Gourmet /></div><p>グルメ</p></li>
+                    <li onClick={() => change_tag("tamasanpo")}><div id="walking"><Walking /></div><p>たまさんぽ</p></li>
+                    <li onClick={() => change_tag("omiyage")}><div id="omiyage"><Souvenir /></div><p>お土産</p></li>
                 </ul>
             </div>
             <div className="pictures-blck">
