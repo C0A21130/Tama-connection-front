ce670258f561b339a9431131a9ec1a54d776ad28
HIRO <c0a2113899@edu.teu.ac.jp>
2022-08-22 10:13:15 +0900

    [update]タブメニュー修正

--- src/components/TabMenu.tsx
+++ src/components/TabMenu.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import "../../static/css/TabMenu.scss"
 
-const TabMenu: React.VFC = () =>{
+const TabMenu: React.FC = () =>{
     return(
         <div className="tab">
             <input id="guide" type="radio" name="tab_item" checked></input>
@@ -12,26 +12,6 @@ const TabMenu: React.VFC = () =>{
             <label className="tab_item" htmlFor="library">ライブラリ</label>
             <input id="ranking" type="radio" name="tab_item"></input>
             <label className="tab_item" htmlFor="ranking">ランキング</label>
-            <div className="tab_content" id="guide_content">
-                <div className="tab_content_description">
-                    <p className="c-txtsp">～～～～～～～ガイドの内容～～～～～～～</p>
-                </div>
-            </div>
-            <div className="tab_content" id="map_content">
-                <div className="tab_content_description">
-                    <p className="c-txtsp">～～～～～～～マップの内容～～～～～～～</p>
-                </div>
-            </div>
-            <div className="tab_content" id="library_content">
-                <div className="tab_content_description">
-                    <p className="c-txtsp">～～～～～～ライブラリの内容～～～～～～</p>
-                </div>
-            </div>
-            <div className="tab_content" id="ranking_content">
-                <div className="tab_content_description">
-                    <p className="c-txtsp">～～～～～～ランキングの内容～～～～～～</p>
-                </div>
-            </div>
         </div>
     )
 }
