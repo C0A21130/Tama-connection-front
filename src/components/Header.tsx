7ac93fde510c15b5ea8f8eaba8ee33435d73727a
HIRO <c0a2113899@edu.teu.ac.jp>
2022-08-22 12:43:48 +0900

    [add]header、tab画像挿入

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import "./../../static/css/Header.scss";
 
+import User from "../../static/images/user.svg"
+
 const head: React.FC = () =>{
     return(
         <div id="header">
         <header>
             <div className="header-block">
-                <p>アイコン</p>
-                <input type="text" placeholder="検索したいもの"/>
+                <p className="icon"><User /></p>
+                <input type="text" className="window" placeholder="検索したいもの"/>
                 <input type="submit" value="検索"/>
             </div>
         </header>
--- src/components/TabMenu.tsx
+++ src/components/TabMenu.tsx
@@ -1,17 +1,23 @@
 import * as React from "react"
 import "../../static/css/TabMenu.scss"
 
+import Guide from "../../static/images/menu/book-opened.svg";
+import Calendar from "../../static/images/menu/calendar.svg";
+import Camera from "../../static/images/menu/camera.svg";
+import Map from "../../static/images/menu/map.svg";
+
+
 const TabMenu: React.FC = () =>{
     return(
         <div className="tab">
             <input id="guide" type="radio" name="tab_item" checked></input>
-            <label className="tab_item" htmlFor="guide">ガイド</label>
+            <label className="tab_item" htmlFor="guide"><div><Guide /></div>ガイド</label>
             <input id="map" type="radio" name="tab_item"></input>
-            <label className="tab_item" htmlFor="map">マップ</label>
+            <label className="tab_item" htmlFor="map"><div><Map /></div>マップ</label>
             <input id="library" type="radio" name="tab_item"></input>
-            <label className="tab_item" htmlFor="library">ライブラリ</label>
+            <label className="tab_item" htmlFor="library"><Camera /><br></br>ライブラリ</label>
             <input id="ranking" type="radio" name="tab_item"></input>
-            <label className="tab_item" htmlFor="ranking">ランキング</label>
+            <label className="tab_item" htmlFor="ranking"><Calendar /><br></br>ランキング</label>
         </div>
     )
 }
