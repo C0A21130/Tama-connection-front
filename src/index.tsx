48bf77b4778fbb91a02019966d53ae3e290ddbb7
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-24 01:02:55 +0900

    [update] react-routerの導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 import App from "./App";
 
 ReactDOM.render(
-    <App />, 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
     document.querySelector("#root")
 )
