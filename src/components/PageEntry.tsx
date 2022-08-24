8d2a0f9a881f41576c8dd25aee1553df85864934
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-24 21:32:29 +0900

    [update] 画像を読み取れるように変更 Base64に変更された画像データを読み込めるように変更

--- src/components/PageEntry.tsx
+++ src/components/PageEntry.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
+import { NavLink } from "react-router-dom";
 
 interface PageProps {
-    image: string
-    title: string
+    page: number,
+    image: string,
+    title: string,
     text: string
 }
 
 const PageEntty: React.FC<PageProps> = (props)=>{
-    const {image, title, text} = props
+    const {page, image, title, text} = props
 
     return(
         <div className="page-entry">
-            <div id="image">{image}</div>
+            <div id="image"><img src={image} /></div>
             <div id="text">
                 <h2>{title}</h2>
                 <p>{text}</p>
