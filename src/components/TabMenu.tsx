ea2b2857880bc0043d769261ddd3a573f305fd38
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-24 19:15:17 +0900

    [update] 画像(svg)ファイルの修正

--- src/components/TabMenu.tsx
+++ src/components/TabMenu.tsx
@@ -1,23 +1,20 @@
 import * as React from "react"
 import "../../static/css/TabMenu.scss"
 
-import Guide from "../../static/images/menu/book-opened.svg";
-import Calendar from "../../static/images/menu/calendar.svg";
-import Camera from "../../static/images/menu/camera.svg";
-import Map from "../../static/images/menu/map.svg";
-
+import Guide from "./../../static/images/tab_menu/guide.svg";
+import Map from "./../../static/images/tab_menu/map.svg";
+import Library from "../../static/images/tab_menu/library.svg";
+import Ranking from "../../static/images/tab_menu/ranking.svg";
 
 const TabMenu: React.FC = () =>{
     return(
-        <div className="tab">
-            <input id="guide" type="radio" name="tab_item" checked></input>
-            <label className="tab_item" htmlFor="guide"><div><Guide /></div>ガイド</label>
-            <input id="map" type="radio" name="tab_item"></input>
-            <label className="tab_item" htmlFor="map"><div><Map /></div>マップ</label>
-            <input id="library" type="radio" name="tab_item"></input>
-            <label className="tab_item" htmlFor="library"><Camera /><br></br>ライブラリ</label>
-            <input id="ranking" type="radio" name="tab_item"></input>
-            <label className="tab_item" htmlFor="ranking"><Calendar /><br></br>ランキング</label>
+        <div className="tab-menu">
+            <ul>
+                <li><div><Guide /></div><p>ガイド</p></li>
+                <li><div><Map /></div><p>マップ</p></li>
+                <li><div><Library /></div><p>ライブラリ</p></li>
+                <li><div><Ranking /></div><p>ランキング</p></li>
+            </ul>
         </div>
     )
 }
