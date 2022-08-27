732bdd4c71681b3f10616c1eeaf1e51c1cdcfc3b
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-27 18:05:59 +0900

    [update] タイトルとタグの情報を取得

--- src/Routes/PostPage.tsx
+++ src/Routes/PostPage.tsx
@@ -2,19 +2,23 @@ import * as React from "react";
 import "./../../static/css/postpage.scss";
 
 const PostPaage: React.FC = () => {
+    const [title, setTitle] = React.useState<string>("たいとる");
+    const [tag, setTag] = React.useState("kankou");
+
     return(
         <div className="post-page">
             <div className="post-page-block">
+                <div id="submit-button">送信</div>
                 <div className="title-block">
                     <label>タイトル</label>
-                    <input type="text"/>
+                    <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
                 </div>
                 <div className="picture-block">
                     <label>写真<input type="file" /></label>
                 </div>
                 <div className="select-tag-block">
                     <label>タグを選択</label>
-                    <select>
+                    <select value={tag} onChange={(event) => setTag(event.target.value)}>
                         <option value="kankou">観光</option>
                         <option value="gurume">グルメ</option>
                         <option value="tamasanpo">たまさんぽ</option>
