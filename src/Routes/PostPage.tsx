7a959c329a75b2f0ac62567a65dec2e315445628
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-27 15:48:57 +0900

    [update] html要素の追加とcssの読み込み設定

--- src/Routes/PostPage.tsx
+++ src/Routes/PostPage.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
+import "./../../static/css/postpage.scss";
 
 const PostPaage: React.FC = () => {
     return(
         <div className="post-page">
-            <h1>PostPage</h1>
+            <div className="post-page-block">
+                <div>
+                    <label>タイトル<input /></label>
+                </div>
+                <div>
+                    <label>写真<input type="file" /></label>
+                </div>
+                <div>
+                    <label>タグを選択
+                        <select>
+                            <option value="kankou">観光</option>
+                            <option value="gurume">グルメ</option>
+                            <option value="tamasanpo">たまさんぽ</option>
+                            <option value="omiyage">お土産</option>
+                        </select>
+                    </label>
+                </div>
+                <div>
+                    <label>説明<input type="text" /></label>
+                </div>
+            </div>
         </div>
     )
 }
