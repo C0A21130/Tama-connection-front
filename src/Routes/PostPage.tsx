19eb0f6519200e5d2f4fa0c60701a3a60e50023d
yuudai65 <c0b210041b@edu.teu.ac.jp>
2022-08-28 15:44:08 +0900

    [update]投稿ページを更新

--- src/Routes/PostPage.tsx
+++ src/Routes/PostPage.tsx
@@ -1,23 +1,26 @@
 import * as React from "react";
 import "./../../static/css/postpage.scss";
 
-const PostPaage: React.FC = () => {
-    const [title, setTitle] = React.useState<string>("たいとる");
+const PostPage: React.FC = () => {
+    const [title, setTitle] = React.useState<string>("");
     const [tag, setTag] = React.useState("kankou");
 
     return(
         <div className="post-page">
             <div className="post-page-block">
-                <div id="submit-button">送信</div>
                 <div className="title-block">
-                    <label>タイトル</label>
-                    <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+                    <label>タイトル
+                    <div><input type="text" placeholder="タイトル" value={title} onChange={(event) => setTitle(event.target.value)}></input></div>
+                    </label>
                 </div>
+
                 <div className="picture-block">
-                    <label>写真<input type="file" /></label>
+                    <label>メインの写真<input type="file" /></label>
                 </div>
+
                 <div className="select-tag-block">
                     <label>タグを選択</label>
+                    <br></br>
                     <select value={tag} onChange={(event) => setTag(event.target.value)}>
                         <option value="kankou">観光</option>
                         <option value="gurume">グルメ</option>
@@ -25,12 +28,24 @@ const PostPaage: React.FC = () => {
                         <option value="omiyage">お土産</option>
                     </select>
                 </div>
+
                 <div className="text-block">
-                    <label>説明<textarea /></label>
+                    <label>説明</label>
+                    <br></br>
+                    <textarea name="comment" cols={20} rows={5}></textarea>
+                </div> 
+                <div className="picture-block">
+                    <label>写真を追加する<input type="file" /></label>
+                </div>
+
+                <div className="submit-button">
+                    <label>送信する</label>
+                    <br></br>
+                    <button  id="test" type="submit">送信</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default PostPaage;
+export default PostPage;
