640dbb23c14f0a91917454c5948fb66065a21d47
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-31 01:30:33 +0900

    [add] チェックポイントをチェックするためのページを追加

---
+++ src/pages/CheckPoint.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const CheckPoint: React.FC = () => {
+    return (
+        <div className="check-point">
+            <h1>チェックポイント</h1>
+        </div>
+    )
+}
+
+export default CheckPoint;
