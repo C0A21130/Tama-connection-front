049eaf5c8232f1050c8abc60cc93bd8c5932962a
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-31 02:48:33 +0900

    [update] ライブラリの店舗コード入力欄を作成

--- src/pages/GetMedal.tsx
+++ src/pages/GetMedal.tsx
@@ -4,7 +4,16 @@ import "./../../static/css/library.scss"
 const GetMedal: React.FC = () => {
     return (
         <div className="get-medal">
-            <h1>チェックポイント</h1>
+            <div className="get-medal-block">
+                <h2>店舗コードを入力しよう!!</h2>
+                <div className="input-number-box">
+                    <label>店舗コード</label>
+                    <input type="text" value={1000}/>
+                </div>
+                <div>
+                    <p>成功</p>
+                </div>
+            </div>
         </div>
     )
 }
