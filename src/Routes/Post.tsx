38f372abf52ddc993736b0998ed4492912b6db39
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-31 21:03:22 +0900

    [update] 投稿ページのデザインを変更

--- src/Routes/Post.tsx
+++ src/Routes/Post.tsx
@@ -5,10 +5,18 @@ const Post: React.FC = () => {
     return (
         <div className="post">
             <h1>投稿</h1>
+            <div className="post-block">
             <ul>
-                <li><Link to="/post/page">写真の投稿</Link></li>
-                <li>投稿された記事</li>
+                <li>
+                    <Link to="/post/page">
+                        <p>写真の投稿</p>
+                    </Link>
+                </li>
+                <li>
+                    <p>投稿された記事</p>
+                </li>
             </ul>
+            </div>
             <Outlet />
         </div>
     )
