4b1fbcf15a7c7e7aa7febcb6a886730cfd03fa93
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-08-31 20:04:14 +0900

    [update] お店を表すコードから自身が入力された値が存在するか確認する機能の追加

--- src/pages/GetMedal.tsx
+++ src/pages/GetMedal.tsx
@@ -1,17 +1,47 @@
 import * as React from "react";
 import "./../../static/css/library.scss"
 
+type StateValues = "" | "成功"  | "失敗";
+
+// (shopId:コード, shopName:お店の名前)
+interface ShopDataType {
+    shopId: number,
+    shopName: string
+}
+
+// お店コードを作成
+const shopDatas: ShopDataType[] = [
+    {shopId: 1, shopName: "test1"},
+    {shopId: 2, shopName: "test2"}
+]
+
 const GetMedal: React.FC = () => {
+    const [inputNum, setInputNum] = React.useState<string>("1");
+    const [state, setState] = React.useState<StateValues>("");
+
+    // 用意されたお店のコードの中に入力された値が存在するか確認
+    const checkShop = ():void => {
+        const d = shopDatas.filter((shopData)=>{
+            return shopData.shopId == Number(inputNum)
+        })
+
+        setState((d.length == 1) ? "成功" : "失敗" )
+        
+    }
+
     return (
         <div className="get-medal">
             <div className="get-medal-block">
                 <h2>店舗コードを入力しよう!!</h2>
                 <div className="input-number-box">
                     <label>店舗コード</label>
-                    <input type="text" value={1000}/>
+                    <input type="text" value={inputNum.toString()} onChange={(event) => setInputNum(event.target.value)}/>
                 </div>
                 <div>
-                    <p>成功</p>
+                    <button onClick={() => checkShop()}>送信</button>
+                </div>
+                <div>
+                    <p>{state}</p>
                 </div>
             </div>
         </div>
