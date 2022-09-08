46f58b7fef95abc8a645b68b66c473dd8ed9ced7
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-09-08 20:07:14 +0900

    [update] タグ名をカタカナから漢字に変換するように変更

--- src/components/CheckPageBlock.tsx
+++ src/components/CheckPageBlock.tsx
@@ -8,12 +8,24 @@ interface PageProps {
 }
 
 const CheckPageBlock: React.FC<PageProps> = (props) => {
-    const { title, image, tag, text } = props
+    const { title, image, tag, text } = props;
+    let tagName;
+    switch (tag) {
+        case "kankou":
+            tagName = "観光"; break;
+        case "gurume":
+            tagName = "グルメ"; break;
+        case "tamasanpo":
+            tagName = "たまさんぽ"; break;
+        case "omiyage":
+            tagName = "お土産"; break;
+    }
+
     return (
         <div className="check-page-block">
             <p>{title}</p>
             <div><img src={image}></img></div>
-            <p>タグ：{tag}</p>
+            <p>タグ：{tagName}</p>
             <p>{text}</p>
         </div>
     )
