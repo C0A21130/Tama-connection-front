7232ed6da0fc8be8231ee94dfdd0f8fa056017a5
yuudai65 <c0b210041b@edu.teu.ac.jp>
2022-09-15 16:12:22 +0900

    [add]メダル確認機能の追加（通信失敗時）

--- src/pages/CheckMedal.tsx
+++ src/pages/CheckMedal.tsx
@@ -1,11 +1,63 @@
 import * as React from "react";
+import axios, { AxiosRequestConfig } from "axios";
+import {constant} from "./../constant"
+
+interface Page {
+    file_name: number,
+    title: string,
+    tag: string,
+    text: string,
+    user: number,
+    location: {
+        x: number,
+        y: number
+    },
+    image: string
+}
+
+
+interface Data {
+    error: string,
+    name: string,
+    checked: number[],
+    files: Page[] | null
+}
+
+const config: AxiosRequestConfig = {
+    headers: {
+        "token": localStorage.getItem("token")
+    }
+}
 
 const CheckMedal: React.FC = () => {
+    const [testData, setTestData] = React.useState<Data>() 
+    React.useEffect(()=>{
+        axios.get<Data>(`${constant.ROOT_URL}/user`, config)
+        .then()
+        .catch((error)=>{
+            console.log(error)
+            const data:Data = {
+                error : "test",
+                name : "username",
+                checked : [5],
+                files: null, 
+            }     
+            setTestData(data)
+            console.log(testData)
+        })
+    },[])
+
     return (
         <div className="check-medal">
             <h1>メダルを確認する</h1>
+            <div>
+                {testData?.checked.map((c) => {
+                    return <div>{c}</div>
+                })}
+            </div>
         </div>
     )
 }
 
 export default CheckMedal;
+
