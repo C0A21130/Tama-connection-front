679c6bd0525247364c64b56ec1fe142424c84758
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-09-17 20:47:37 +0900

    [update] DBにお店のIDを登録

--- src/pages/GetMedal.tsx
+++ src/pages/GetMedal.tsx
@@ -1,16 +1,45 @@
 import * as React from "react";
+import axios, { AxiosRequestConfig } from "axios";
+import { constant } from "./../constant";
 import checkShop from "./../lib/checkShop"
 
-type StateValues = "" | "成功"  | "失敗";
+type StateValues = "" | "成功"  | "失敗" | "既にチェック済み";
 
 const GetMedal: React.FC = () => {
     const [inputNum, setInputNum] = React.useState<string>("0");
     const [state, setState] = React.useState<StateValues>("");
+    const config: AxiosRequestConfig = {
+        headers: {
+            "token": localStorage.getItem("token")
+        }
+    }
 
-    // 用意されたお店のコードの中に入力された値が存在するか確認
+    // 送信ボタンを押したときの処理
     const getShop = ():void => {
-        const num = checkShop(Number(inputNum)).length;
-        setState(((num==1) ? "成功" : "失敗"))
+        // 用意されたお店のコードの中に入力された値が存在するか確認
+        const shopId: number = Number(inputNum)
+        const num = checkShop(shopId).length;
+
+        // お店のIDがJSONファイルに存在しないときに終了する
+        if (num == 0) {
+            setState("失敗");
+            return 
+        }
+        
+        // ユーザーのチェックポイント情報を更新
+        axios.put(`${constant.ROOT_URL}/user`, {shop_id : shopId}, config)
+        .then((response) => {
+            // 既にチェック済みの場合は失敗を表示
+            if(response.data == "error checked") {
+                setState("既にチェック済み");
+            // 成功した際に画面に成功を表示
+            } else {
+                setState("成功");
+            }
+        })
+        .catch(() => {
+            setState("失敗");
+        })
     }
 
     return (
