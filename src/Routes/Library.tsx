1093a1584e07f795596d60a0b71ddefa9b552a17
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-09-19 17:18:49 +0900

    [fix] Library.scssの変更

--- src/Routes/Library.tsx
+++ src/Routes/Library.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Link, Outlet } from "react-router-dom";
 
-import "./../static/css/library.scss";
+import "./../static/css/Library.scss";
 
 import Restaurant from "./../static/images/library/restaurant.svg";
 import Medal from "./../static/images/library/medal.svg";
