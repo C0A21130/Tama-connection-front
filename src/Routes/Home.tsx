67dafbcbbd9a8acbe62b7c3f932af19588cbe82e
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-10-02 16:30:16 +0900

    [update] ページ数を指定して更新できるように変更

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -12,6 +12,7 @@ import Souvenir from "./../static/images/tag_menu/souvenir.svg";
 import "./../static/css/home.scss";
 
 const ROOT_URL = constant.ROOT_URL;
+const PER_PAGE = 3;
 
 // 投稿されたページ情報の型
 interface PageData {
@@ -36,10 +37,17 @@ interface State {
     pageNum: number
 }
 
+interface Max {
+    "kankou" : number,
+    "gurume" : number,
+    "tamasanpo" : number,
+    "omiyage" : number
+}
+
 // axiosで受け取るデータの型
 interface ResponsePageData {
     result: PageData[],
-    max: number
+    max : Max
 }
 
 // 記事の配列から2つの画像を取り出す
@@ -69,55 +77,83 @@ const Home: React.FC = () => {
 
     // 表示するページと写真ボックスの変数
     const [displayPage, setDisplayPage] = React.useState<PageData[]>();
+    const [maxPageNums, setMaxPageNum] = React.useState<Max>();
     const [picBox, setPicBox] = React.useState<string[]>(["", ""]);
 
     // 表示するページを変える関数
-    const changePage = (tag:Tag, pages: PageData[][], setPages: React.Dispatch<React.SetStateAction<PageData[][]>>, pageNum: number)=> {
-        const num = pageNum * 4;
+    const changePage = (action:State, pages: PageData[][], setPages: React.Dispatch<React.SetStateAction<PageData[][]>>)=> {
         // ページが保存されている場合にはそのまま利用する
         try {
-            if (pages[num][0] == undefined) {
+            console.log(maxPageNums)
+            if (pages[action.pageNum][0] == undefined) {
                 throw new Error("undefind");
             }
-            setDisplayPage(pages[num]);
+            setDisplayPage(pages[action.pageNum]);
         // ページが保存されていない場合はAPIサーバが記事を取得する
         } catch(e) {
-            console.log(e);
-            axios.get<ResponsePageData>(`${ROOT_URL}/pages?tag=${tag}&pageNum=${pageNum}`)
+            axios.get<ResponsePageData>(`${ROOT_URL}/pages?tag=${action.tag}&pageNum=${action.pageNum}`)
             .then((response) => {
                 setDisplayPage(response.data.result);
                 setPages([...pages, response.data.result]);
+                setMaxPageNum(response.data.max);
             })
         }
     }
 
-    // タグと番号を同時変更する変数
+    // タグと番号を同時変更するreducer変数
     const reduser = (state: State, action: State) => {
+        // ページの上限に達していないか確認する変数
+        let flag: boolean;
+
         switch(action.tag){
             case "kankou":
-                changePage(action.tag, kankouData, setKankouData, action.pageNum);
+                flag = (-1 < action.pageNum) && (Math.floor(maxPageNums.gurume / PER_PAGE) >= action.pageNum);
+                // 上限に達していなければページを更新する
+                if (flag) {
+                    changePage(action, kankouData, setKankouData);
+                    return action
+                }
                 break;
             case "gurume":
-                changePage(action.tag, gurumeData, setGurumeData, action.pageNum);
+                flag = (-1 < action.pageNum) && (Math.floor(maxPageNums.gurume / PER_PAGE) >= action.pageNum);
+                // 上限に達していなければページを更新する
+                if (flag) {
+                    changePage(action, gurumeData, setGurumeData);
+                    return action
+                }
                 break;
             case "tamasanpo":
-                changePage(action.tag, tamasanpoData, setTamasanpoData, action.pageNum);
+                flag = (-1 < action.pageNum) && (Math.floor(maxPageNums.tamasanpo / PER_PAGE) >= action.pageNum);
+                // 上限に達していなければページを更新する
+                if (flag) {
+                    changePage(action, tamasanpoData, setTamasanpoData);
+                    return action  
+                }
                 break;
             case "omiyage":
-                changePage(action.tag, omiyageData, setOmiyageData, action.pageNum);
+                flag = (-1 < action.pageNum) && (Math.floor(maxPageNums.omiyage / PER_PAGE) >= action.pageNum);
+                // 上限に達していなければページを更新する
+                if (flag) {
+                    changePage(action, omiyageData, setOmiyageData);
+                    return action
+                }
                 break;
             default:
                 return state;
         }
-        return action
+        return state
     }
     const [state, dispath] = React.useReducer(reduser, init);  
+
+    React.useEffect(() => {
+        changePage(init, kankouData, setKankouData);
+    }, [])
 
     return(
         <div className="home">
             <div className="tag-menu-block">
                 <ul>
-                    <li onClick={() => dispath({tag: "kankou", pageNum:0})} className={state.tag == "kankou" ? "active" : "noactive"}><div className="icon"><Sightseeing /></div><p>観光地</p></li>
+                    <li onClick={() => dispath({tag: "kankou", pageNum: 0})} className={state.tag == "kankou" ? "active" : "noactive"}><div className="icon"><Sightseeing /></div><p>観光地</p></li>
                     <li onClick={() => dispath({tag: "gurume", pageNum: 0})} className={state.tag == "gurume" ? "active" : "noactive"}><div className="icon"><Gourmet /></div><p>グルメ</p></li>
                     <li onClick={() => dispath({tag: "tamasanpo", pageNum: 0})} className={state.tag == "tamasanpo" ? "active" : "noactive"}><div className="icon"><Walking /></div><p>たまさんぽ</p></li>
                     <li onClick={() => dispath({tag: "omiyage", pageNum: 0})} className={state.tag == "omiyage" ? "active" : "noactive"}><div className="icon"><Souvenir /></div><p>お土産</p></li>
@@ -133,6 +169,11 @@ const Home: React.FC = () => {
                 {displayPage?.map((page, index) =>
                     <PageEntry page={page.file_name} title={page.title} text={page.text} image={page.image} key={index} />
                 )}
+            </div>
+            <div>
+                <button onClick={() => { dispath({ tag: state.tag, pageNum: state.pageNum - 1 }) }}>＜</button>
+                <p>{state.pageNum}</p>
+                <button onClick={() => { dispath({ tag: state.tag, pageNum: state.pageNum + 1 }) }}>＞</button>
             </div>
         </div>
     )
