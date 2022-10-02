0810ef4de13c783c80b446817f780f704dd9ce5e
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-10-02 19:50:07 +0900

    [add] フッターの作成

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <div className="footer">
+        <footer>
+            <div className="link-block">
+                <h3>リンク</h3>
+                <a href="https://sites.google.com/d/1FqlL4lFenh7EqO03_cBpDhnI1eUZ_L14/p/1ztR1fQ4chkE8zqWthh7TGH51bjHd2aZV/edit">
+                    <p>ホームページ</p>
+                </a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSf6GxLNn_mkP2j_jzppZb2K2-9hzKOLrAr1XDS2BkVd3TRCew/viewform">
+                    <p>お問い合わせフォーム</p>
+                </a>
+            </div>
+            <div className="tag-block">
+                <h3>タグの説明</h3>
+                <p>たまファーム：収穫体験や収穫した野菜や花を投稿</p>
+                <p>グルメ：多摩地域で食べた料理を投稿</p>
+                <p>たまさんぽ：お散歩中に見つけた新しい発見を投稿</p>
+                <p>お土産：多摩地域で購入したお土産を投稿</p>
+            </div>
+        </footer>
+        </div>
+    )
+}
+
+export default Footer
