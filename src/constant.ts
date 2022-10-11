e7f6dcc5cb98a7328c183d61f43846ae65bc8bd3
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-10-12 00:45:59 +0900

    Revert "[update] URLの変更"
    This reverts commit a60587a85cca6dd391e3f48fee92c4620e1af8ff.

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,5 @@
 
 export namespace constant {
     // export const ROOT_URL = "http://localhost:5000";
-    // export const ROOT_URL = "https://tama-connection-backend.herokuapp.com";
-    export const ROOT_URL = "http://tk2-123-61896.vs.sakura.ne.jp/api";
+    export const ROOT_URL = "https://tama-connection-backend.herokuapp.com";
 }
