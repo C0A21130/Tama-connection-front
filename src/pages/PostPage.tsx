5a6214c7f1506f643161e98368bfcc5bd2897847
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-10-29 01:17:41 +0900

    [update] 写真の投稿の際に位置情報も添付するように変更

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -18,29 +18,44 @@ interface sendBody {
 }
 
 const postPage: React.FC = () => {
+    // 投稿内容を保存する
     const [title, setTitle] = React.useState<string>("");
     const [tag, setTag] = React.useState<string>("kankou");
     const [text, setText] = React.useState<string>("");
     const [pic, setPic] = React.useState<string>("")
     const [status, setStatus] = React.useState<"成功"|"失敗">()
 
+    // 現在地を保存する
+    const [myx, setMyx] = React.useState(0);
+    const [myy, setMyy] = React.useState(0);
+
+    // 画面移動用メソッドを作成
     const navigate = useNavigate();
+    //画像圧縮モジュールの作成
     const render = new FileReader();
+    // 送信のHTTPheaderの設定
     const config: AxiosRequestConfig = {
         headers: {
             "token": localStorage.getItem("token")
         }
     }
+
     const body: sendBody = {
         title: title,
         tag: tag,
         text: text,
         location: {
-            x: -1,
-            y: -1
+            x: myx,
+            y: myy
         },
         image: ""
     }
+
+    // 現在地を取得
+    navigator.geolocation.getCurrentPosition((position) => {
+        setMyx(position.coords.longitude);
+        setMyy(position.coords.latitude);
+    })
 
     // 送信ボタンを押したときの処理
     const submitPage = (submit: boolean) => {
