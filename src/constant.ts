3ff92a30a13acc12b32504c20656c1e090630ef6
C0A21130 <c0a2113061@edu.teu.ac.jp>
2023-02-05 23:39:19 +0900

    [fix] 接続先のAPIサーバーの設定を変更

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
 
 export namespace constant {
-    // export const ROOT_URL = "http://localhost:5000";
-    export const ROOT_URL = "http://tk2-123-61896.vs.sakura.ne.jp/api"
+    export const ROOT_URL = "http://localhost:5000";
+    // export const ROOT_URL = "http://tk2-123-61896.vs.sakura.ne.jp/api"
 }
