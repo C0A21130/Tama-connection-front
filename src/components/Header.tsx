5f5a1095a2eee97015cc4cea9782800fb0bbd353
C0A21130 <c0a2113061@edu.teu.ac.jp>
2023-02-12 01:44:52 +0900

    [update] ヘッダーをクリックした際にホームへ戻るように変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import Icon from "./../static/images/icon.svg";
 
 const head: React.FC = () =>{
     return(
         <div className="header">
+        <Link to="/">
         <header>
             <div className="icon"><Icon /></div>
             <h1>たまこねくしょん</h1>
         </header>
+        </Link>
         </div>
     )
 };
