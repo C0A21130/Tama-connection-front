d0776315450cbd1552b35ca485eac1ca6f5c9259
yuudai65 <c0b210041b@edu.teu.ac.jp>
2023-02-18 13:26:48 +0900

    [update]フッターの並び順を変更説明上にしたほうがわかりやすい気がしたから

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,6 +13,13 @@ const Footer: React.FC = () => {
     return (
         <div className="footer">
         <footer>
+            <div className="tag-block">
+                <h3>タグの説明</h3>
+                <p>たまファーム：収穫体験や収穫した野菜、農地の美しい自然を投稿</p>
+                <p>グルメ：多摩地域で食べた料理を投稿</p>
+                <p>たまさんぽ：お散歩中に見つけた新しい発見を投稿</p>
+                <p onClick={() => delAccout()}>お土産：多摩地域で購入したお土産を投稿</p>
+            </div>
             <div className="link-block">
                 <h3>関連サイト</h3>
                 <a href="https://www.teu.ac.jp/">
@@ -20,20 +27,13 @@ const Footer: React.FC = () => {
                 </a>
                 <a href="https://sites.google.com/d/1FqlL4lFenh7EqO03_cBpDhnI1eUZ_L14/p/1ztR1fQ4chkE8zqWthh7TGH51bjHd2aZV/edit">
                     <p>公式ホームページ</p>
+                </a>    
+                <a href="https://twitter.com/tama_connection">
+                    <p>公式ツイッター</p>
                 </a>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSeT0MGdGbpRLZp7F63V9n_bZsjm72PS0GcY0cMME92Z1VC9eA/viewform?usp=sf_link">
                     <p>アンケートフォーム</p>
                 </a>
-                <a href="https://twitter.com/tama_connection">
-                    <p>公式ツイッター</p>
-                </a>
-            </div>
-            <div className="tag-block">
-                <h3>タグの説明</h3>
-                <p>たまファーム：収穫体験や収穫した野菜、農地の美しい自然を投稿</p>
-                <p>グルメ：多摩地域で食べた料理を投稿</p>
-                <p>たまさんぽ：お散歩中に見つけた新しい発見を投稿</p>
-                <p onClick={() => delAccout()}>お土産：多摩地域で購入したお土産を投稿</p>
             </div>
         </footer>
         </div>
